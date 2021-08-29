2629829b7275151b8b14fb79a9f12220ddeaf6b9
Phuoc Pham <phuocpham723@gmail.com>
2021-08-29 10:07:59 +0700

    finish project

--- section11/src/components/Cart/Cart.js
+++ section11/src/components/Cart/Cart.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Modal from '../UI/Modal';
+import cartContext from './../../store/cart-context';
 import classes from './Cart.module.css';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const cartCtx = useContext(cartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({
+      ...item,
+      amount: 1,
+    });
+  };
+
+  const cartItemRemodeHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
   const cartItems = (
     <ul className={classes['cart-items']}>
-      {[{ id: 'c1', name: 'Sushi', amount: 2, price: 12.99 }].map((item) => (
-        <li key={item.id}>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          price={item.price}
+          amount={item.amount}
+          onAdd={cartItemAddHandler.bind(null, item)}
+          onRemove={cartItemRemodeHandler.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
@@ -16,13 +40,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onHidenCart}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- section11/src/store/CartContextProvider.js
+++ section11/src/store/CartContextProvider.js
@@ -1,44 +1,22 @@
 import { useReducer } from 'react';
 import CartContext from './cart-context';
-
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Sushi',
-    description: 'Finest fish and veggies',
-    price: 22.99,
-    amount: 0,
-  },
-  {
-    id: 'm2',
-    name: 'Schnitzel',
-    description: 'A german specialty!',
-    price: 16.5,
-    amount: 0,
-  },
-  {
-    id: 'm3',
-    name: 'Barbecue Burger',
-    description: 'American, raw, meaty',
-    price: 12.99,
-    amount: 0,
-  },
-  {
-    id: 'm4',
-    name: 'Green Bowl',
-    description: 'Healthy...and green...',
-    price: 18.99,
-    amount: 0,
-  },
-];
+import _ from 'lodash';
 
 const defaultCartState = {
-  items: DUMMY_MEALS,
+  items: [],
   totalAmount: 0,
 };
 
 const CART_ADD_ACTION = 'CART_ADD_ACTION';
 const CART_REMOVE_ACTION = 'CART_REMOVE_ACTION';
+
+const findExistingItem = (items, targetId) => {
+  const targetIndex = _.findIndex(items, (item) => {
+    return item.id === targetId;
+  });
+
+  return items[targetIndex];
+};
 
 const cartReducer = (state, action) => {
   let updatedTotalAmount;
@@ -48,52 +26,52 @@ const cartReducer = (state, action) => {
       updatedTotalAmount =
         state.totalAmount + action.item.price * action.item.amount;
 
-      const indexCartItemIndex = state.items.findIndex(
-        (item) => item.id === action.item.id
-      );
+      const targetItem = findExistingItem(state.items, action.item.id);
 
-      const existingCartItem = state.items[indexCartItemIndex];
+      if (targetItem) {
+        const updatedItems = [...state.items];
 
-      let updatedItems;
+        const targetIndex = _.findIndex(updatedItems, (item) => {
+          return item.id === targetItem.id;
+        });
 
-      if (existingCartItem) {
-        const updatedItem = {
-          ...existingCartItem,
-          amount: existingCartItem.amount + action.item.amount,
+        updatedItems[targetIndex] = {
+          ...targetItem,
+          amount: targetItem.amount + action.item.amount,
         };
-        updatedItems = [...state.items];
-        updatedItems[indexCartItemIndex] = updatedItem;
-      } else {
-        updatedItems = state.items.concat(action.item);
-      }
 
-      return {
-        items: updatedItems,
-        totalAmount: updatedTotalAmount,
-      };
+        return { items: updatedItems, totalAmount: updatedTotalAmount };
+      } else {
+        const updatedItems = state.items.concat(action.item);
+        return { items: updatedItems, totalAmount: updatedTotalAmount };
+      }
 
     case CART_REMOVE_ACTION:
-      const existingCartItemIndex = state.items.findIndex(
-        (item) => item.id === action.id
-      );
-      const existingItem = state.items[existingCartItemIndex];
-      updatedTotalAmount = state.totalAmount - existingItem.price;
-      let newItems;
-      if (existingItem.amount === 1) {
-        newItems = state.items.filter((item) => item.id !== action.id);
-      } else {
-        const updatedItem = {
-          ...existingItem,
-          amount: existingItem.amount - 1,
-        };
-        newItems = [...state.items];
-        newItems[existingCartItemIndex] = updatedItem;
-      }
+      const removeTargetItem = findExistingItem(state.items, action.id);
+      updatedTotalAmount = state.totalAmount - removeTargetItem.price;
 
-      return {
-        items: updatedItems,
-        totalAmount: updatedTotalAmount,
-      };
+      if (removeTargetItem.amount === 1) {
+        return {
+          items: state.items.filter((item) => item.id !== removeTargetItem.id),
+          totalAmount: updatedTotalAmount,
+        };
+      } else {
+        const updatedItems = [...state.items];
+
+        const targetIndex = _.findIndex(updatedItems, (item) => {
+          return item.id === removeTargetItem.id;
+        });
+
+        updatedItems[targetIndex] = {
+          ...removeTargetItem,
+          amount: removeTargetItem.amount - 1,
+        };
+
+        return {
+          items: updatedItems,
+          totalAmount: updatedTotalAmount,
+        };
+      }
 
     default:
       return defaultCartState;
